7a34e75d55b9210bad3c0a4dfd291054765ebe6c
Alepell289 <77011578+Alepell289@users.noreply.github.com>
2021-01-07 17:22:54 +0100

    Delete funkcja.sql

--- ZALICZENIE_BAZ_DANYCH-main/funkcja.sql
+++
@@ -1,8 +0,0 @@
-SET GLOBAL log_bin_trust_function_creators = 1;
-DELIMITER ** 
-CREATE FUNCTION ilosc_ksiazek_ponizej_jakiejs_ceny(n int) 
-RETURNS INT 
-BEGIN 
-SELECT COUNT(cena) FROM ksiazki WHERE cena<n into n;
-RETURN n; 
-END**
