f7711671e63738cd33066ef3a2cf1ed83823897f
Alepell289 <77011578+Alepell289@users.noreply.github.com>
2021-01-07 17:23:31 +0100

    Delete procedura.sql

--- ZALICZENIE_BAZ_DANYCH-main/procedura.sql
+++
@@ -1,7 +0,0 @@
-DELIMITER %% 
-CREATE PROCEDURE najnowszezamowienie (out najnowsze date)
-BEGIN 
-SELECT MAX(data) into najnowsze FROM zamowienia;
-END; 
-CALL najnowszezamowienie (@m);%%
-SELECT @m;
